0887587eb0db8d578d52f51e756463515eec6495
Olga Hajduk <hajduk.o.a@gmail.com>
2021-02-04 15:46:02 +0300

    feat: add solve for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    const stringReversed = n.toString().split('').reverse().join('')
+    const nReversed = parseInt(stringReversed)
+
+    return nReversed
 }
